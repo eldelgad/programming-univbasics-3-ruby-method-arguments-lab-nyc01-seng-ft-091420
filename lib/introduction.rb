1b66f974c2b211dcb68fa78c8a6b5e229fe855b9
Elias Delgado <evdelgado99@gmail.com>
2020-08-25 22:46:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,9 +3,17 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+##### PSEUDOCODE ##### 
+
+# defined method: introduction (takes name parameter)
+# outputs string: "Hi, my name is #{name}"
+
+def introduction(name)
+  puts "Hi, my name is #{name}"
+end
 
 
-
+#### PSEUDOCODE ####
 
 
 
