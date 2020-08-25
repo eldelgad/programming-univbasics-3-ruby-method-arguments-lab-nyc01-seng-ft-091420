ced4ed212db1fa1ea57c5ae0cc18f9b0554c1baa
Elias Delgado <evdelgado99@gmail.com>
2020-08-25 22:52:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,14 +15,15 @@ end
 
  ##### PSEUDOCODE ##### 
 
-# defined method: introduction_with_language_optional (takes 2 parameters: name, language = "Ruby")
+# defined method: introduction_with_language (takes 2 parameters: name, language)
 # outputs string: "Hi, my name is #{name} and I am learning to program in #{language}."
 
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+# defined method: introduction_with_language_optional (takes 2 parameters: name, language = "Ruby")
+# outputs string: "Hi, my name is #{name} and I am learning to program in #{language}."
 
 def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
